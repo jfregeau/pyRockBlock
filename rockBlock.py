b3a30113ee7d19dac6c6d1e468cdebfa85cc4ff8
jfregeau <43116885+jfregeau@users.noreply.github.com>
2019-07-22 16:12:44 -0700

    Update rockBlock.py

--- rockBlock.py
+++ rockBlock.py
@@ -312,7 +312,7 @@ class rockBlock(object):
                 
         if( len(msg) > 340):
                
-            print "sendMessageWithBytes bytes should be <= 340 bytes"
+            print("sendMessageWithBytes bytes should be <= 340 bytes")
             
             return False
         
@@ -564,7 +564,7 @@ class rockBlock(object):
           
         if( response == "OK" ):
         
-            print "No message content.. strange!"
+            print("No message content.. strange!")
             
             if(self.callback != None and callable(self.callback.rockBlockRxReceived) ): 
                 self.callback.rockBlockRxReceived(mtMsn, "")
